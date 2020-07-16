b4e9abf3b5f3ec13ebcf0a3d48d79289f5bbed0a
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-16 20:21:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday_supplies_hash|
     if season == :winter
+      holiday_supplies_hash.each do |
 
 end
 
