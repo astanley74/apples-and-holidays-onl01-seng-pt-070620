af06e813cdf87f945b563076b03377cd51caaeba
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-16 21:24:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize:}"
     holidays.each do |holiday, supplies|
-      puts "#{holiday.to_s.split("_").collect do |"
+      puts "#{holiday.to_s.split("_").collect do |supply| supply.capitalize"
 end
 
 def all_holidays_with_bbq(holiday_hash)
