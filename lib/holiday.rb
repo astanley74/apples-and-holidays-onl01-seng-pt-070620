95694680c00814193299e15fc9ce970cd9c4e379
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-16 20:45:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[]
+  holiday_hash[:fall][:halloween] = ["Pumpkins"]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
