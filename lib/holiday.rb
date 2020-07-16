d33c6c9a356f2918aa0e0c6489c5813a2e36758f
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-16 20:57:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,6 +47,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].collect do |holiday, supplies|
     supplies
   end
+  supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
