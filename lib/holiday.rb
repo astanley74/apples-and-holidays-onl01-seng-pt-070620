996b5ad5e3344fd2de821fe598d67541fae7f3b4
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-16 21:26:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,10 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holidays|
-    puts "#{season.to_s.capitalize:}"
+holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday.to_s.split("_").collect { |supply| supply.capitalize }.join(' ') }: #{supplies.join(", ")}"
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end
   end
 end
