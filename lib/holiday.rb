86682a91aa34f534d4381d682b50f289795070b9
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-16 20:35:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,8 +35,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
  holiday_hash[:spring].each do |holiday, supplies|
    supplies << "Table Cloth"
-   supplies << ""
-   
+   supplies << "Grill"
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
